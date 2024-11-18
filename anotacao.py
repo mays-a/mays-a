71aa7dba5b5fa1ac8553ae53a75d6d30c18f5fc9
mays-a <o.maysa@aluno.ifsp.edu.br>
2024-11-18 14:11:57 -0300

    Update anotacao.py

--- anotacao.py
+++ anotacao.py
@@ -1,4 +1,21 @@
 teste=True
-x=float(input('Digite um número: '))
-r=x*x
-print('Resposta: ', r)
+x=float(input('Digite um número: ')) #float inclui números com vírgula
+h=22 #nn faz sentido
+
+if x>2 and x<5:
+    pass
+if x<=2 or x>=5:
+    pass
+if not(x>2 and x<5):
+    pass
+
+#-----------------------------------------------------------------
+
+if x>0 and x<=20:
+    if h>20: #if alinhado, um dentro do outro
+        r=x*x
+        print('Resposta: ', r)
+elif h> 20: #nn faz sentido, nn tem coorrelção
+    print('')
+else:
+    print('Número inválido,deveria ser maior que zero.')
